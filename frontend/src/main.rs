ee6a58332b4baf5a7d3233c7821dcca29f8c3bb6
Khuu Viet Sang <sangkhuudev@gmail.com>
2024-05-31 10:16:54 +0700

    Setup websocket client

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,52 @@
+use web_sys::HtmlTextAreaElement;
+use yew::prelude::*;
+use yew_hooks::use_websocket;
+
+#[function_component]
+fn App() -> Html {
+    let message_handle = use_state(Vec::default);
+    let message = (*message_handle).clone();
+    let mut cloned_message = message.clone();
+
+    let new_message_handle = use_state(String::default);
+    let new_message = (*new_message_handle).clone();
+    let cloned_new_message = new_message.clone();
+
+    let ws = use_websocket("ws://127.0.0.1:8000/".to_string());
+
+    use_effect_with(ws.message.clone(), move |ws_message| {
+        if let Some(msg) = &**ws_message {
+            cloned_message.push(msg.clone());
+            message_handle.set(cloned_message);
+        }
+    });
+
+    let cloned_new_message_handle = new_message_handle.clone();
+    let on_message_change = Callback::from(move |e: Event| {
+        let target = e.target_dyn_into::<HtmlTextAreaElement>();
+        if let Some(t) = target {
+            cloned_new_message_handle.set(t.value());
+        }
+    });
+
+    let cloned_ws = ws.clone();
+    let on_button_click = Callback::from(move |_: MouseEvent| {
+        cloned_ws.send(cloned_new_message.clone());
+        new_message_handle.set("".to_string());
+    });
+    html! {
+        <>
+            <ul id="chat">
+                {
+                    message.iter().map(|m| html!{<li>{m}</li>}).collect::<Html>()
+                }
+            </ul>
+            <textarea onchange={on_message_change} value={new_message}></textarea>
+            <button type="submit" onclick={on_button_click}>{"Send"}</button>
+        </>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
